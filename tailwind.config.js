503aedc3da1c15c77f211d5496e337f9a5330e73
kabir shuvo <kabirshuvo19@gmail.com>
2024-01-13 10:52:49 +0600

    Tailwind.config file updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      screens: {
+        sm: '100%',
+        md: '100%',
+        lg: '1024px',
+        xl: '1280px',
+        '2xl': '1400px',
+      },
+    },
+    colors: {
+      light: '#ffffff',
+      dark: '#111111',
+      primary: '#EBF3F3',
+      secondary: '#F6FCFF',
+      teal: '#357E8D',
+      warn: '#ff0000',
+      green: '#00866a',
+      transparent: '#ffffff00',
+    },
   },
   plugins: [],
-}
-
+};
